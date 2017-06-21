9a525a1226a0374bd8e7cf4ac6f4d5b35b319723
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-06-21 23:21:52 +0900

    modify data

--- linear-regression/predict-longevity.R
+++ linear-regression/predict-longevity.R
@@ -1,1 +1,21 @@
+library(ggplot2)
+
 ages <- read.csv("longevity.csv")
+
+gplo <- ggplot(ages, aes(x = AgeAtDeath, fill = factor(Smokes))) +
+　geom_density() +
+　facet_grid(Smokes ~ .)
+
+plot(gplo)
+
+# 以下、ネットで見つけたggplotのサンプルコード
+# http://motw.mods.jp/R/ggplot_tutorial.html
+# irisという、rのプリセットのデータを利用している
+# library(ggplot2)
+# str(iris)
+#
+# p <- ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width))
+# q <- p + geom_point()
+# ggsave(file="graph1.png")
+#
+# plot(q)
