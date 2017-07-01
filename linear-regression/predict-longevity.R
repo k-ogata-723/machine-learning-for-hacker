2df7a1a8114ddc4cf8ab7f3f6c51e75c5895c803
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-07-01 23:51:08 +0900

    Add officail sample code

--- linear-regression/predict-longevity.R
+++ linear-regression/predict-longevity.R
@@ -2,28 +2,46 @@ library(ggplot2)
 
 ages <- read.csv("longevity.csv")
 
-guess.accuracy <- data.frame()
+constant.guess <- with(ages, mean(AgeAtDeath))
+with(ages, sqrt(mean((AgeAtDeath - constant.guess) ^ 2)))
 
-for (guess in seq(63, 83, by = 1)) {
-  prediction.error <- with(ages,
-                      mean((AgeAtDeath - guess) ^ 2))
-  guess.accuracy <- rbind(guess.accuracy,
-                          data.frame(Guess = guess,
-                                    Error = prediction.error))
-}
+smokers.guess <- with(subset(ages, Smokes == 1),
+                      mean(AgeAtDeath))
+non.smokers.guess <- with(subset(ages, Smokes == 0),
+                      mean(AgeAtDeath))
 
-# forで回して目的関数を作成
-gplo <- ggplot(guess.accuracy, aes(x = Guess, y = Error)) +
-　geom_point() +
-　geom_line()
+ages <- transform(ages,
+                  NewPrediction = ifelse(Smokes == 0,
+                                  non.smokers.guess,
+                                  smokers.guess))
+with(ages, sqrt(mean((AgeAtDeath - NewPrediction) ^ 2)))
 
-# 読み込んだデータをそのままプロット
+## guessを63から83まで回して、目的関数を作成
+# guess.accuracy <- data.frame()
+#
+# for (guess in seq(63, 83, by = 1)) {
+#   prediction.error <- with(ages,
+#                       mean((AgeAtDeath - guess) ^ 2))
+#   guess.accuracy <- rbind(guess.accuracy,
+#                           data.frame(Guess = guess,
+#                                     Error = prediction.error))
+# }
+#
+# # forで回して目的関数を作成
+# gplo <- ggplot(guess.accuracy, aes(x = Guess, y = Error)) +
+# 　geom_point() +
+# 　geom_line()
+##
+
+## 読み込んだデータをそのままプロット
 # fillでグラフないを色付け
 # facet_gridでグラフをSmokesが0の時と1のときで分ける
 # gplo <- ggplot(ages, aes(x = AgeAtDeath, fill = factor(Smokes))) +
 # 　geom_density() +
 # 　facet_grid(Smokes ~ .)
+##
 
+print(ages)
 plot(gplo)
 
 # 以下、ネットで見つけたggplotのサンプルコード
