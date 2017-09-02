06b5adb64f3ad84c731a9da1c0b2226fe9faa1fe
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-09-02 23:02:43 +0900

    Liner regression

--- 6.regularization/gam.R
+++ 6.regularization/gam.R
@@ -19,3 +19,39 @@ ggplot(data.frame(XSquared = x.squared, Y = y), aes(x = XSquared, y = Y)) +
   geom_smooth(method = 'lm', se = FALSE)
 
 #### 以下、非線形のデータを多項式回帰する
+set.seed(1)
+
+x <- seq(0, 1, by = 0.01)
+# 正弦波を使って、非線形のデータを作成する
+y <- sin(2 * pi * x) + rnorm(length(x), 0, 0.1)
+
+df <- data.frame(X = x, Y = y)
+
+ggplot(df, aes(x = X, y = Y)) +
+  geom_point()
+
+非線形のデータに、直線を当てはめてみる
+summary(lm(Y ~ X, data = df))
+
+Call:
+lm(formula = Y ~ X, data = df)
+
+# Residuals:
+#      Min       1Q   Median       3Q      Max
+# -1.00376 -0.41253 -0.00409  0.40664  0.85874
+#
+# Coefficients:
+#             Estimate Std. Error t value Pr(>|t|)
+# (Intercept)  0.94111    0.09057   10.39   <2e-16 ***
+# X           -1.86189    0.15648  -11.90   <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+# Residual standard error: 0.4585 on 99 degrees of freedom
+# 明らかにうまく行かないはずなのに、60%の変動が説明できている
+# Multiple R-squared:  0.5885,	Adjusted R-squared:  0.5843
+# F-statistic: 141.6 on 1 and 99 DF,  p-value: < 2.2e-16
+
+ggplot(df, aes(x = X, y = Y)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE)
