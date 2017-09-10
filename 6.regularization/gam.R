951d2aa709e386e348c724c8366114159def1825
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-09-10 15:44:51 +0900

    Make RMSE

--- 6.regularization/gam.R
+++ 6.regularization/gam.R
@@ -55,3 +55,106 @@ lm(formula = Y ~ X, data = df)
 ggplot(df, aes(x = X, y = Y)) +
   geom_point() +
   geom_smooth(method = 'lm', se = FALSE)
+
+df <- transform(df, X2 = x ^ 2)
+df <- transform(df, X3 = x ^ 3)
+
+summary(lm(Y ~ X + X2 + X3, data = df))
+
+# Call:
+# lm(formula = Y ~ X + X2 + X3, data = df)
+#
+# Residuals:
+#      Min       1Q   Median       3Q      Max
+# -0.32331 -0.08538  0.00652  0.08320  0.20239
+#
+# Coefficients:
+#              Estimate Std. Error t value Pr(>|t|)
+# (Intercept)  -0.16341    0.04425  -3.693 0.000367 ***
+# X            11.67844    0.38513  30.323  < 2e-16 ***
+# X2          -33.94179    0.89748 -37.819  < 2e-16 ***
+# X3           22.59349    0.58979  38.308  < 2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+# Residual standard error: 0.1153 on 97 degrees of freedom
+# Xの２乗や3乗を追加したことで、r-squaredが58%から97%に上がった
+# ただし、この方法方法では、データをどんどん追加していくとデータの相関が強すぎて予測ができなくなっていく
+# Multiple R-squared:  0.9745,	Adjusted R-squared:  0.9737
+# F-statistic:  1235 on 3 and 97 DF,  p-value: < 2.2e-16
+
+上記のデータを増やした時の相関をなくすために、poly関数で
+summary(lm(Y ~ poly(X, degree = 14), data = df))
+
+# poly関数を使って、相関の弱い形でデータの次数を増やす
+# ploy関数のdegreeで次数を設定できる
+# ただ、次数を増やしすぎるとover fittingになる
+poly.fit <- lm(Y ~ poly(X, degree = 7), data = df)
+df <- transform(df, PredictY = predict(poly.fit))
+
+ggplot(df, aes(x = X, y = PredictY)) +
+  geom_point() +
+  geom_line()
+
+
+# データを学習データとテストデータに分けて、交差検定をおこなう
+# まず、交差検定のための正弦波のseedを作成していく
+
+set.seed(1)
+
+x <- seq(0, 1, by = 0.01)
+y <- sin(2 * pi * x) + rnorm(length(x), 0, 0.1)
+
+print(y)
+
+n <- length(x)
+
+# データをtraingデータとtestデータにランダムな割合に分割するためにindicesを生成
+indices <- sort(sample(1:n, round(0.5 * n)))
+
+# 始めからindices個のデータをトレーニングデータとして利用する
+training.x <- x[indices]
+training.y <- y[indices]
+
+print(training.x)
+
+test.x <- x[-indices]
+test.y <- y[-indices]
+
+# テストデータとトレーニングデータのデータフレームを作成する
+# rでは、data.frameは色々な方が入れられて、matrixは1種類の型しか入れられないらしい
+training.df <- data.frame(X = training.x, Y = training.y)
+test.df <- data.frame(X = test.x, Y = test.y)
+
+# 以下、RMSE（平均２乗誤差、root mean square error）のコードを作成する
+# degree（関数の次元）を増やしていって、テストデータとトレーニングデータの学習曲線を比較
+# まず、コストを出す関数を自作
+rmse <- function(y, h)
+{
+  return(sqrt(mean((y - h) ^ 2)))
+}
+
+performance <- data.frame()
+
+# degreeを1から12まで増やしつつ、計算していく
+for (d in 1:12)
+{
+  poly.fit <- lm(Y ~ poly(X, degree = d), data = training.df)
+
+  performance <- rbind(performance,
+                      data.frame(Degree = d,
+                                  Data = 'Training',
+                                  RMSE = rmse(training.y, predict(poly.fit))))
+
+  performance <- rbind(performance,
+                      data.frame(Degree = d,
+                                  Data = 'Test',
+                                  RMSE = rmse(test.y, predict(poly.fit,
+                                              newdata = test.df))))
+}
+
+ggplot(performance, aes(x = Degree, y = RMSE, linetype = Data)) +
+  geom_point() +
+  geom_line()
+
+  
